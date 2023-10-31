5b784dc278f18e90f762cdef27d73767e1c2d462
Miguel <d.elputoamor@edu.gva.es>
2023-10-31 13:08:07 +0100

    Pagina 20 del pdf

--- app/src/main/java/com/pmdm/saludapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.pmdm.saludapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnHello: AppCompatButton = findViewById(R.id.bntHello)
+
+        btnHello.setOnClickListener {
+            Log.i("Test", "Botón Pulsado")
+        }
+
+        }
     }
-}
