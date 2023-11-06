69e381642a0ee28a89b5b7fc40c260fa112a2cdd
Miguel <d.elputoamor@edu.gva.es>
2023-11-06 12:46:17 +0100

    fINALIZACIÓN DE LA plicación

--- app/src/main/java/com/pmdm/saludapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludapp/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.pmdm.saludapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val he = findViewById<EditText>(R.id.etName)
         val btnHello: AppCompatButton = findViewById(R.id.bntHello)
 
         btnHello.setOnClickListener {
-            Log.i("Test", "Botón Pulsado")
+            //Log.i("Test", "Botón Pulsado")
+          var name = he.text.toString()
+
+
+            val intentGA = Intent(this, GreetingActivity::class.java)
+            intentGA.putExtra("EXTRA_NAME", name)
+            startActivity(intentGA)
+
+
         }
 
         }
